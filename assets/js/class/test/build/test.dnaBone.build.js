20fcb9cb755e5076db73c0450971578bb2520637
bemuse01 <sequeland@gmail.com>
2022-05-27 10:56:28 +0900

    update

--- assets/js/class/test/build/test.dnaBone.build.js
+++ assets/js/class/test/build/test.dnaBone.build.js
@@ -1,11 +1,14 @@
 import * as THREE from '../../../lib/three.module.js'
 import Particle from '../../objects/particle.js'
+import Shader from '../shader/test.dnaBone.shader.js'
 
 export default class{
     constructor({group}){
         this.group = group
 
         this.group.rotation.z = -90 * RADIAN
+
+        this.particles = []
 
         this.init()
     }
@@ -33,8 +36,8 @@ export default class{
         this.group.add(finalGroup)
     }
     createBone(finalGroup){
-        const degree = 2
-        const count = 100
+        const degree = 8
+        const count = 30
         const maxY = 50
         const stepY = maxY / (count - 1)
         const radius = 10
@@ -56,10 +59,16 @@ export default class{
         }
 
         const particle = new Particle({
-            materialName: 'PointsMaterial',
+            materialName: 'ShaderMaterial',
             materialOpt: {
-                size: 0.75,
-                color: 0xffffff
+                vertexShader: Shader.vertex,
+                fragmentShader: Shader.fragment,
+                transparent: true,
+                uniforms: {
+                    uColor: {value: new THREE.Color(0xffffff)},
+                    uPointSize: {value: 16.0},
+                    uTime: {value: 0}
+                }
             }
         })
 
@@ -72,5 +81,16 @@ export default class{
     // animate
     animate(){
         this.group.rotation.x += 0.01
+
+        this.updateUniforms('rightGroup')
+        this.updateUniforms('leftGroup')
+    }
+    updateUniforms(groupName){
+        const time = window.performance.now()
+
+        this[groupName].children.forEach(particle => {
+            const uniforms = particle.material.uniforms
+            uniforms['uTime'].value = time
+        })
     }
 }
---
+++ assets/js/class/test/shader/test.dnaBone.shader.js
@@ -0,0 +1,43 @@
+import ShaderMethod from '../../../method/method.shader.js'
+
+export default {
+    vertex: `
+        uniform float uPointSize;
+
+        varying vec3 vPosition;
+
+        void main(){
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            gl_PointSize = uPointSize;
+
+            vPosition = position;
+        }
+    `,
+    fragment: `
+        uniform vec3 uColor;
+        uniform float uTime;
+
+        varying vec3 vPosition;
+
+        ${ShaderMethod.snoise3D()}
+        ${ShaderMethod.executeNormalizing()}
+
+        void main(){
+            float f = length(gl_PointCoord - vec2(0.5));
+            
+            float r = snoise3D(vec3(vPosition.xz * 10.0, uTime * 0.001));
+            float n = executeNormalizing(r, 0.35, 0.5, -1.0, 1.0);
+            float n2 = executeNormalizing(r, 0.1, 0.35, -1.0, 1.0);
+
+            if(f > n){
+                discard;
+            }
+
+            if(f < n2){
+                discard;
+            }
+
+            gl_FragColor = vec4(uColor, 1.0);
+        }
+    `
+}
