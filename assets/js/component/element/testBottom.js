9caa8f84307c997358ef9855441355b5a6938d5b
bemuse01 <sequeland@gmail.com>
2022-06-20 11:26:49 +0900

    update

--- assets/js/component/element/testBottom.js
+++ assets/js/component/element/testBottom.js
@@ -6,6 +6,9 @@ const generateStyle = (size, gap) => {
         animation: `blink ${Math.random() * 2 + 2}s infinite`
     }
 }
+const genRand = (max) => {
+    return ~~(Math.random() * max)
+}
 
 export default {
     template: `
@@ -29,7 +32,64 @@ export default {
 
                 </div>
 
-                <div class="bottom-texts">
+                <div class="bottom-child-texts bottom-texts">
+                    
+                    <div class="bottom-texts-box">
+
+                        <div
+                            v-for="text in texts"
+                            :key="text.key"
+                            class="bottom-text"
+                        >
+                            <p
+                                v-for="child in text.childs"
+                                :key="child.key"
+                            >{{child.text}}
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="bottom-child-texts bottom-texts2">
+                    
+                    <div class="bottom-texts2-box">
+
+                        <div
+                            v-for="text in texts2"
+                            :key="text.key"
+                            class="bottom-text bottom-text2"
+                        >
+                            <p
+                                v-for="child in text.childs"
+                                :key="child.key"
+                            >{{child.text}}
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="bottom-child-texts bottom-texts3">
+                        
+                    <div class="bottom-texts3-box">
+
+                        <div
+                            v-for="text in texts3"
+                            :key="text.key"
+                            class="bottom-text bottom-text3"
+                        >
+                            <p
+                                v-for="child in text.childs"
+                                :key="child.key"
+                            >{{child.text}}
+                            </p>
+                        </div>
+
+                    </div>
+
                 </div>
 
                 <div class="bottom-empty">
@@ -43,7 +103,7 @@ export default {
         const {ref, onMounted} = Vue
 
         const size = 5
-        const gap = 1
+        const gap = 1.5
 
         const root = ref()
         const circlesRef = ref()
@@ -51,6 +111,27 @@ export default {
         const circles = ref(Array.from({length: 0}, (_, key) => ({
             key,
             style: generateStyle(size, gap)
+        })))
+        const texts = ref(Array.from({length: 3}, (_, key) => ({
+            key,
+            childs: Array.from({length: 5}, (_, idx) => ({
+                key: idx,
+                text: key !== 0 && idx === 4 ? '' : genRand(10000)
+            }))
+        })))
+        const texts2 = ref(Array.from({length: 2}, (_, key) => ({
+            key,
+            childs: Array.from({length: 3}, (_, idx) => ({
+                key: idx,
+                text: idx === 2 ? `${genRand(100000)}/${genRand(10000)}` : genRand(100)
+            }))
+        })))
+        const texts3 = ref(Array.from({length: 2}, (_, key) => ({
+            key,
+            childs: Array.from({length: 3}, (_, idx) => ({
+                key: idx,
+                text: genRand(1000)
+            }))
         })))
 
         const updateCircles = () => {
@@ -66,11 +147,6 @@ export default {
             }else{
                 for(let i = 0; i < count2 - len; i++) circles.value.push({style: generateStyle(size, gap), key: len + i})
             }
-
-            console.log('w: ', w)
-            console.log('width: ', width)
-            console.log('count2: ', count2)
-            console.log('len: ', len)
         }
 
         const resize = () => {
@@ -85,7 +161,10 @@ export default {
         return{
             root,
             circlesRef,
-            circles
+            circles,
+            texts,
+            texts2,
+            texts3
         }
     }
 }
