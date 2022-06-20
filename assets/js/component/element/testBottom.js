5f18d26a25518613f94160a3c41960bd60413e2b
bemuse01 <sequeland@gmail.com>
2022-06-20 14:10:08 +0900

    update

--- assets/js/component/element/testBottom.js
+++ assets/js/component/element/testBottom.js
@@ -32,67 +32,31 @@ export default {
 
                 </div>
 
-                <div class="bottom-child-texts bottom-texts">
-                    
-                    <div class="bottom-texts-box">
-
-                        <div
-                            v-for="text in texts"
-                            :key="text.key"
-                            class="bottom-text"
-                        >
-                            <p
-                                v-for="child in text.childs"
-                                :key="child.key"
-                            >{{child.text}}
-                            </p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="bottom-child-texts bottom-texts2">
-                    
-                    <div class="bottom-texts2-box">
-
-                        <div
-                            v-for="text in texts2"
-                            :key="text.key"
-                            class="bottom-text bottom-text2"
-                        >
-                            <p
-                                v-for="child in text.childs"
-                                :key="child.key"
-                            >{{child.text}}
-                            </p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="bottom-child-texts bottom-texts3">
+                <div 
+                    class="bottom-child-texts bottom-texts"
+                    v-for="text in texts"
+                    :key="text.key"
+                    :style="text.rootStyle"
+                >
                         
-                    <div class="bottom-texts3-box">
+                    <div class="bottom-texts-box" :style="text.boxStyle">
 
                         <div
-                            v-for="text in texts3"
-                            :key="text.key"
-                            class="bottom-text bottom-text3"
+                            class="bottom-text bottom-text"
+                            v-for="row in text.rows"
+                            :key="row.key"
+                            :style="text.itemStyle"
                         >
-                            <p
-                                v-for="child in text.childs"
-                                :key="child.key"
-                            >{{child.text}}
-                            </p>
+                            <span
+                                v-for="col in row.cols"
+                                :key="col.key"
+                                :style="text.textStyle"
+                            >{{col.text}}
+                            </span>
                         </div>
 
                     </div>
 
-                </div>
-
-                <div class="bottom-empty">
                 </div>
 
             </div>
@@ -112,27 +76,62 @@ export default {
             key,
             style: generateStyle(size, gap)
         })))
-        const texts = ref(Array.from({length: 3}, (_, key) => ({
-            key,
-            childs: Array.from({length: 5}, (_, idx) => ({
-                key: idx,
-                text: key !== 0 && idx === 4 ? '' : genRand(10000)
-            }))
-        })))
-        const texts2 = ref(Array.from({length: 2}, (_, key) => ({
-            key,
-            childs: Array.from({length: 3}, (_, idx) => ({
-                key: idx,
-                text: idx === 2 ? `${genRand(100000)}/${genRand(10000)}` : genRand(100)
-            }))
-        })))
-        const texts3 = ref(Array.from({length: 2}, (_, key) => ({
-            key,
-            childs: Array.from({length: 3}, (_, idx) => ({
-                key: idx,
-                text: genRand(1000)
-            }))
-        })))
+
+        const texts = ref([
+            {
+                key: 1,
+                boxStyle: {width: '90%', gap: '3%'},
+                rows: Array.from({length: 3}, (_, key) => ({
+                    key,
+                    cols: Array.from({length: 5}, (_, idx) => ({
+                        key: idx,
+                        text: key !== 0 && idx === 4 ? '' : genRand(10000)
+                    }))
+                }))
+            },
+            {
+                key: 2,
+                rootStyle: {marginLeft: '6%'},
+                itemStyle: {gap: '25%'},
+                rows: Array.from({length: 2}, (_, key) => ({
+                    key,
+                    cols: Array.from({length: 3}, (_, idx) => ({
+                        key: idx,
+                        text: idx === 2 ? `${genRand(100000)}/${genRand(10000)}` : genRand(100)
+                    }))
+                }))
+            },
+            {
+                key: 3,
+                rootStyle: {marginLeft: '4%', flex: '1'},
+                itemStyle: {gap: '15%'},
+                textStyle: {display: 'inline-block'},
+                rows: Array.from({length: 2}, (_, key) => ({
+                    key,
+                    cols: Array.from({length: 3}, (_, idx) => ({
+                        key: idx,
+                        text: genRand(1000)
+                    }))
+                }))
+            },
+            {
+                key: 4,
+                rootStyle: {flex: '0.45'},
+                rows: [
+                    {
+                        key: 0,
+                        cols: [{key: 0, text: 'WAVEFORM_DATA'}]
+                    },
+                    {
+                        key: 1,
+                        cols: Array.from({length: 2}, (_, idx) => ({
+                            key: idx,
+                            text: genRand(100)
+                        }))
+                    }
+                ]
+            }
+        ])
 
         const updateCircles = () => {
             const {width} = circlesRef.value.getBoundingClientRect()
@@ -163,8 +162,6 @@ export default {
             circlesRef,
             circles,
             texts,
-            texts2,
-            texts3
         }
     }
 }
