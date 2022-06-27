33173128b99f1f6aadaf8e7244f7e6ecc3678210
bemuse01 <sequeland@gmail.com>
2022-06-27 14:36:41 +0900

    Update barGraphBack.js

--- assets/js/component/element/barGraphBack.js
+++ assets/js/component/element/barGraphBack.js
@@ -18,15 +18,15 @@ export default {
                 <v-rows class="barGraph-back-rows" gap="2%">
 
                     <v-row flex="none" width="12px">
-                        <axisY width="80%" pointHeight="2px" :len="5" gap="23%" />
+                        <axisY :style="leftAxisStyle" width="80%" pointHeight="2px" :len="5" gap="23%" />
                     </v-row>
 
                     <v-row flex="1">
-                        <barGraph-back-bars />
+                        <barGraph-back-bars :style="barsStyle" />
                     </v-row>
 
                     <v-row flex="none" width="12px">
-                        <axisY width="80%" pointHeight="2px" :len="5" gap="23%" justifyContent="flex-end" />
+                        <axisY :style="rightAxisStyle" width="80%" pointHeight="2px" :len="5" gap="23%" justifyContent="flex-end" />
                     </v-row>
             
                 </v-rows>
@@ -34,5 +34,33 @@ export default {
             </div>
 
         </div>
-    `
+    `,
+    setup(){
+        const {ref, onMounted, computed} = Vue
+        const {useStore} = Vuex
+
+        const store = useStore()
+
+        const openTime = computed(() => store.getters['test/getOpenTime'])
+
+        const leftAxisStyle = ref({opacity: '0', animation: 'none'})
+        const rightAxisStyle = ref({opacity: '0', animation: 'none'})
+        const barsStyle = ref({opacity: '0', animation: 'none'})
+
+        const open = () => {
+            leftAxisStyle.value.animation = `blink2 0.06s ${openTime.value + Math.random()}s 2 forwards`
+            rightAxisStyle.value.animation = `blink2 0.06s ${openTime.value + Math.random()}s 2 forwards`
+            barsStyle.value.animation = `blink2 0.06s ${openTime.value + Math.random()}s 2 forwards`
+        }
+
+        onMounted(() => {
+            open()
+        })
+
+        return{
+            leftAxisStyle,
+            rightAxisStyle,
+            barsStyle
+        }
+    }
 }
